d344ce1fc1b4a9b1b68e518812d6b970f6e6b327
Tue Jensen <tuebjensen@gmail.com>
2024-05-24 23:02:35 +0200

    remove rate from main loop

--- src/tracker_node.py
+++ src/tracker_node.py
@@ -17,8 +17,8 @@ class TrackerNode:
         self.model = YOLO(sys.argv[1])
         self.model.to("cuda")
   
-        self.pub = rospy.Publisher('detection_data', String, queue_size=10)
-        self.rate = rospy.Rate(20)
+        self.obj_pub = rospy.Publisher('detection_data', String, queue_size=10)
+        #self.img_pub = rospy.Publisher('annotated_image', Image, queue_size=10)
 
         self.depth_img_sub = rospy.Subscriber('/camera/depth/image_raw', Image, self.depth_img_callback)
         self.color_img_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.color_img_callback)
@@ -26,7 +26,8 @@ class TrackerNode:
         self.depth_img_updated = False
         self.color_img = None
         self.color_img_updated = False
-        self.msg = String()
+        self.obj_msg = String()
+        self.img_msg = Image()
 
         self.get_kinect_position_srv = rospy.ServiceProxy('gazebo/get_model_state', GetModelState)
 
@@ -78,23 +79,23 @@ class TrackerNode:
         p_world = cam_to_world @ np.concatenate([p_cam, [1]])
         return p_world[:3]/p_world[3]
 
-    def boxes_to_json(self, boxes):
+    def bboxes_to_json(self, bboxes):
         msg_data = []
-        boxes_cls = boxes.cls.int().cpu().tolist()
-        boxes_label = [classes[i] for i in boxes_cls]
-        boxes_conf = boxes.conf.cpu().tolist()
+        bboxes_cls = bboxes.cls.int().cpu().tolist()
+        bboxes_label = [classes[i] for i in bboxes_cls]
+        bboxes_conf = bboxes.conf.cpu().tolist()
         tracking_ids = None
-        boxes_xywh = boxes.xywh.cpu().tolist()
-        if boxes.id is not None:
-            tracking_ids = boxes.id.int().cpu().tolist()
-        for i in range(len(boxes_cls)):
+        bboxes_xywh = bboxes.xywh.cpu().tolist()
+        if bboxes.id is not None:
+            tracking_ids = bboxes.id.int().cpu().tolist()
+        for i in range(len(bboxes_cls)):
             box = {}
-            box['class'] = boxes_cls[i]
-            box['label'] = boxes_label[i]
-            box['confidence'] = boxes_conf[i]
+            box['class'] = bboxes_cls[i]
+            box['label'] = bboxes_label[i]
+            box['confidence'] = bboxes_conf[i]
             box['tracking_id'] = tracking_ids[i] if tracking_ids is not None else None
-            box['boundingBox'] = {'x': boxes_xywh[i][0], 'y': boxes_xywh[i][1], 'w': boxes_xywh[i][2], 'h': boxes_xywh[i][3]}
-            world_coordinates = self.pixel_to_world(int(boxes_xywh[i][0]), int(boxes_xywh[i][1]))
+            box['boundingBox'] = {'x': bboxes_xywh[i][0], 'y': bboxes_xywh[i][1], 'w': bboxes_xywh[i][2], 'h': bboxes_xywh[i][3]}
+            world_coordinates = self.pixel_to_world(int(bboxes_xywh[i][0]), int(bboxes_xywh[i][1]))
             box['worldCoordinates'] = {'x': world_coordinates[0], 'y': world_coordinates[1], 'z': world_coordinates[2]}
             msg_data.append(box)
 
@@ -106,16 +107,18 @@ class TrackerNode:
                 continue
 
             results = self.model.track(self.color_img, persist=True, verbose=False, classes=[0, 1], conf=0.5)
-            boxes = results[0].boxes
+            bboxes = results[0].boxes
 
             # annotated_frame = results[0].plot()
             # cv2.imwrite('annotated_frame.jpg', annotated_frame)
             
-            self.msg.data = self.boxes_to_json(boxes)
-            self.pub.publish(self.msg)
+            self.obj_msg.data = self.bboxes_to_json(bboxes)
+            # self.img_msg = self.cv_bridge.cv2_to_imgmsg(annotated_frame, encoding="bgr8")
+            self.obj_pub.publish(self.obj_msg)
+            # self.img_pub.publish(self.img_msg)
             self.color_img_updated = False
             self.depth_img_updated = False 
-            self.rate.sleep()
+            rospy.loginfo('Published detection data and annotated image')
 
 
 if __name__ == '__main__':
